584acb17782efac5c9a44ecca5780daf6430406d
yarnserve <yarnserve@gmail.com>
2023-03-05 11:45:34 +0900

    jquery

--- 17_magnific_popup/app.js
+++ 17_magnific_popup/app.js
@@ -10,4 +10,52 @@ $(function () {
     e.preventDefault()
     $.magnificPopup.close()
   })
+
+  // youtube
+  $('.popup-youtube').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false,
+  })
+
+  //   ajax
+  $('.simple-ajax-popup').magnificPopup({
+    type: 'ajax',
+  })
+
+  // single lightbox
+  $('.image-popup-vertical-fit').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    mainClass: 'mfp-img-mobile',
+    image: {
+      verticalFit: true,
+    },
+  })
+
+  $('.image-popup-fit-width').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    image: {
+      verticalFit: false,
+    },
+  })
+
+  $('.image-popup-no-margins').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+    image: {
+      verticalFit: true,
+    },
+    zoom: {
+      enabled: true,
+      duration: 300, // don't foget to change the duration also in CSS
+    },
+  })
 })
