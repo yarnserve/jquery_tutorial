e1410da6671fd40ed09307b2be1c6794a84b77b7
yarnserve <yarnserve@gmail.com>
2023-03-05 11:55:11 +0900

    jquery

--- 17_magnific_popup/app.js
+++ 17_magnific_popup/app.js
@@ -58,4 +58,23 @@ $(function () {
       duration: 300, // don't foget to change the duration also in CSS
     },
   })
+
+  //   gallery
+  $('.gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function (item) {
+        return item.el.attr('title') + '<small>by yarnserve</small>'
+      },
+    },
+  })
 })
